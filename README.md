1e09dbe085f78305741e39967e22ba254807ed93
Matthew Jones <mwjones0896@gmail.com>
2025-10-17 18:41:29 +0100

    Extract schema from each JSON

---
+++ README.md
@@ -0,0 +1,33 @@
+Medly backend API running on Docker
+
+# Stage 1: Data Normalisation
+
+## Raw data
+
+### User Data
+| userId | createdAt | questionId | isMarked |
+| - | - | - | - | - |
+
+### Canvas Data
+
+| CanvasId | 
+
+### Course Data
+| Name | Age |
+| - | - |
+| Matt | 28 |
+| Liz | 32 |
+
+### Practice Data
+| Name | Age |
+| - | - |
+| Matt | 28 |
+| Liz | 32 |
+
+### Exam Data
+| Name | Age |
+| - | - |
+| Matt | 28 |
+| Liz | 32 |
+
+## First Normal Form (1NF)
---
+++ create_schema.py
@@ -0,0 +1,40 @@
+import json
+from genson import SchemaBuilder
+
+def main():
+    input = [
+        'user_data/user_data.json', 
+        'curriculum_data/aqaGCSEBio_course.json', 
+        'curriculum_data/aqaGCSEBio_exams.json', 
+        'curriculum_data/aqaGCSEBio_practices.json'
+    ]
+    output = [
+        'user_data/user_data_schema.json', 
+        'curriculum_data/aqaGCSEBio_course_schema.json', 
+        'curriculum_data/aqaGCSEBio_exams_schema.json', 
+        'curriculum_data/aqaGCSEBio_practices_schema.json'
+    ]
+    
+    io = zip(input, output)
+    input, output = next(io)
+    builder = SchemaBuilder()
+
+    with open(input, "r") as f:
+        data = json.load(f)
+        for v in data["targets"].values():
+            builder.add_object(v)
+    
+    with open(output, "w") as f:
+        f.write(json.dumps(builder.to_schema(), indent=2))
+        
+    for i, o in io:
+        builder = SchemaBuilder()
+        with open(i, "r") as f:
+            data = json.load(f)
+            builder.add_object(data)
+        
+        with open(o, "w") as f:
+            f.write(json.dumps(builder.to_schema(), indent=2))
+
+if __name__ == "__main__":
+    main()
